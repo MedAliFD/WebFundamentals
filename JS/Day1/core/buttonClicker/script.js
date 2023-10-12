23fc6ffc61bf3ed61b76965efea356f77c15a4bf
MedAliFD <medalifaddaoui@gmail.com>
2023-10-12 18:55:27 +0100

    button click function

---
+++ JS/Day1/core/buttonClicker/script.js
@@ -0,0 +1,15 @@
+// remove element
+function removing(element)
+    {element.remove();
+}
+
+// inner text changing
+function logout(element) 
+{
+    element.innerText = "Logout";
+}
+
+// display allert
+function displayAlert(element){
+    alert('Ninja was liked');
+}
