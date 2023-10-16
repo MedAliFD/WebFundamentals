8bd3043fe20d5a94415bceca47cc9d12bbd28dd4
MedAliFD <medalifaddaoui@gmail.com>
2023-10-16 20:22:03 +0100

    multiple like button solution

--- JS/Day2/core/script.js
+++ JS/Day2/core/script.js
@@ -1,16 +1,3 @@
-var like1 = document.getElementById("test1");
-var like2 = document.getElementById("test2");
-var like3 = document.getElementById("test3");
-console.log(like1.innerText);
-console.log(like2.innerText);
-console.log(like3.innerText);
 function liking1(element){
     like1.innerText++
 }
-function liking2(element) {
-    like2.innerText++
-}
-function liking3(element) {
-    like3.innerText++
-}
-
